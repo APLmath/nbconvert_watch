84e79b0667da17323d2ec8c0da7c98c384fee2de
Andrew Lee <andrew.lee.cal@gmail.com>
2016-09-28 19:22:00 -0700

    raw_input() works better

--- nbconvert_watch/command_line.py
+++ nbconvert_watch/command_line.py
@@ -5,7 +5,7 @@ def main():
     if len(sys.argv) != 3:
         print('Welcome to nbconvert-watch! Please run "nbconvert-watch <notebook folder> <results folder>"')
         print('Press Enter to continue...')
-        input()
+        raw_input()
         return 0
 
     notebook_dir, results_dir = sys.argv[1:3]
